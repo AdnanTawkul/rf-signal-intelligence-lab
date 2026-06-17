44823389d105f94c21174305abfeaa625be2e363
Adnan Tawkul <adnan.tawkul@gmail.com>
2026-06-17 20:22:42 +0200

    Add SimCLR contrastive training engine

--- src/rfsil/ssl/__init__.py
+++ src/rfsil/ssl/__init__.py
@@ -8,12 +8,20 @@ from rfsil.ssl.contrastive import (
     SimCLRModel,
     nt_xent_loss,
 )
+from rfsil.ssl.training import (
+    ContrastiveEpochMetrics,
+    run_contrastive_evaluation_epoch,
+    run_contrastive_training_epoch,
+)
 
 __all__ = [
+    "ContrastiveEpochMetrics",
     "IQAugmentationConfig",
     "ProjectionHead",
     "ProjectionHeadConfig",
     "RandomIQAugmentation",
     "SimCLRModel",
     "nt_xent_loss",
+    "run_contrastive_evaluation_epoch",
+    "run_contrastive_training_epoch",
 ]
